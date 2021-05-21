a9cffcec345091ec5dac57dbf522286f4eb798f0
Spencer Gray <spencergray666@hotmail.com>
2021-05-20 18:46:29 -0700

    added column to ContextFunctionRow

--- src/contextTypes.ts
+++ src/contextTypes.ts
@@ -9,5 +9,5 @@ export type TableDataEntry = TableDataEntryArray | TableDataEntryJSON;
 // context helper
 export type ContextFunctionData<R> = (data: TableData) => R
 export type ContextFunctionCell<R> = (value: any, row: TableDataEntryArray | TableDataEntryJSON, key: string | number, data: TableData, column: ColumnOption, index?: number) => R
-export type ContextFunctionRow<R> = (row: TableDataEntryArray | TableDataEntryJSON, key: string | number, data: TableData, index?: number) => R
+export type ContextFunctionRow<R> = (row: TableDataEntryArray | TableDataEntryJSON, key: string | number, data: TableData, column: ColumnOption, index?: number) => R
 export type ContextFunctionPagination<R> = (pageSize: number, pageIndex: number, goToPage: (page: number) => void, data: TableData) => R
